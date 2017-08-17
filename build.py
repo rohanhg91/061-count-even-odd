8f7463b897ae4250331d2881a51d4eab15fee547
rohanhg91 <rohanhgupta91@gmail.com>
2017-08-17 08:34:22 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,19 @@
 def solution(list_of_nums):
     """Enter Code Here"""
+    even = []
+    odd = []
+
+    for i in list_of_nums:
+        if i % 2 == 0:
+            even.append(i)
+        else:
+            odd.append(i)
+
+    num_even = len(even)
+    num_odd = len(odd)
+    return {'ODD': num_odd, 'EVEN': num_even}
+
+'''
+a = solution([1,2,3,4,5,6,7])
+print a
+'''
